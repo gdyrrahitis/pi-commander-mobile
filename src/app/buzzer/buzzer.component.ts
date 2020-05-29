d48898d1454d8723b22ecc11f95ed2d4c1e62a6c
George <giorgos.dyrrahitis@outlook.com>
2020-05-29 17:01:49 +0100

    updated buzzer component

--- src/app/buzzer/buzzer.component.ts
+++ src/app/buzzer/buzzer.component.ts
@@ -9,19 +9,36 @@ import * as Toast from "nativescript-toast";
 })
 export class BuzzerComponent implements OnInit {
 
+  private states: { [key: string]: string } = {
+    "idle": "Buzz",
+    "active": "Wait..."
+  };
+
+  private error = false;
+  private enabled = true;
+  private text: string;
+
   constructor(private service: BuzzerService) { }
 
   ngOnInit(): void {
-  	// TODO: security - oauth
+    // TODO: security - oauth
+    this.text = this.states["idle"];
   }
 
-
   onTap() {
+    this.text = this.states["active"];
+    this.enabled = false;
   	this.service.postBuzzerCommand().subscribe(response => {
-  		console.log(response);
-  		Toast.makeText("Buzzer's gonna buzz...", "long").show();
+      Toast.makeText("Buzzer's gonna buzz...").show();
+      this.text = this.states["idle"];
+      this.enabled = true;
   	}, error => {
-  		console.log(error);
+      this.error = true;
+      setTimeout(() => {
+        this.error = false;
+        this.text = this.states["idle"];
+        this.enabled = true;
+      }, 1500);
   		Toast.makeText("Something's wrong, try later :/", "long").show();
   	});
   }
